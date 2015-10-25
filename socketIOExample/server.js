d3df691f7a5f90101c55bcaadd3b249c477dfa9d
Ayush Gera <ayushgera@Ayushs-MacBook-Pro.local>
2015-10-25 23:54:03 +0530

    socket IO integration first commit

---
+++ socketIOExample/server.js
@@ -0,0 +1,22 @@
+
+//
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+server.listen(process.env.PORT || 8082);
+
+io.sockets.on('connection', function (socket) {
+  socket.on('location', function (data) {
+    io.sockets.emit('location', data);
+  });
+});
+
+// For serving static files inside ./client
+app.use(require('express').static(__dirname + '/client'));
+
+// For hosing on Heroku 
+/*io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+  io.set('log level', 1)
+});*/
